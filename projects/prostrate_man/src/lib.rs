6c4b610dd9a9b9f27a0cd49868adbf2dbaeb6342
SalC124 <Saltcal124@gmail.com>
2025-05-13 11:12:08 -0400

    basic post functionality added. this was actual torture

--- projects/prostrate_man/src/lib.rs
+++ projects/prostrate_man/src/lib.rs
@@ -1,10 +1,13 @@
+use reqwest::Client;
 use serde_json::Value;
 use std::error::Error;
 
 pub struct Config {
+    pub client: Client,
     pub req_type: String,
     pub url: String,
-    pub req: serde_json::Value,
+    pub req: Value,
+    pub bearer: String,
 }
 
 impl Config {
@@ -13,23 +16,58 @@ impl Config {
             return Err("not enough arguments".into());
         }
 
+        let client = reqwest::Client::new();
+
         let req_type: String = args[1].clone();
         let url: String = args[2].clone();
-        let req: Value = serde_json::from_str(&args[3].clone()).map_err(Box::new)?;
-        Ok(Config { req_type, url, req })
+        let req: serde_json::Value = serde_json::from_str(&args[3].clone()).unwrap();
+        // let req: Value = serde_json::from_str(&args[3].clone()).map_err(Box::new)?;
+        let bearer: String = if args.len() > 4 {
+            args[4].clone()
+        } else {
+            String::from("")
+        };
+        Ok(Config {
+            client,
+            req_type,
+            url,
+            req,
+            bearer,
+        })
     }
 }
 
 pub async fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let res = match config.req_type.as_str() {
         "get" => get(config).await?,
-        _ => serde_json::from_str(r#"{"error":"your response type isnt valid"}"#).map_err(Box::new)?,
+        "post" => post(config).await?,
+        _ => serde_json::from_str(r#"{"error":"your response type isnt valid"}"#)
+            .map_err(Box::new)?,
     };
-    println!("{res:#?}");
+    let skib: String = res["token"].to_string();
+    println!("{}", skib);
+    println!("{:#?}", res);
     Ok(())
 }
 
 async fn get(config: Config) -> Result<Value, Box<dyn Error>> {
-    let res = reqwest::get(config.url).await?.json::<Value>().await?;
+    let res = config
+        .client
+        .get(config.url)
+        .send()
+        .await?
+        .json::<Value>()
+        .await?;
+    Ok(res)
+}
+async fn post(config: Config) -> Result<Value, Box<dyn Error>> {
+    let res = config
+        .client
+        .post(config.url)
+        .json(&config.req)
+        .send()
+        .await?
+        .json::<Value>()
+        .await?;
     Ok(res)
 }
